05dc686cd25fb92fc472ef49df1f8732cfffccc1
Bibek Thapa <thapabibek1989@gmail.com>
2025-03-21 19:14:44 +0545

    folder restructure backend

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,6 @@ app.include_router(login.router, prefix="/users", tags=["Users"])
 app.include_router(register.router, prefix="/users", tags=["Users"])
 app.include_router(shutdown.router, prefix="/events", tags=["Events"])
 
-@app.get("/",tags=['Root    '])
+@app.get("/",tags=['Root'])
 async def root():
     return {"message": "Welcome to the Language Learning API"}
