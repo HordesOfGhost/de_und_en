c9074aa3a14b7ac97359fdae9796964545d29040
Bibek Thapa <thapabibek1989@gmail.com>
2025-04-14 22:37:15 +0200

    fix frontend audio play

--- routers/synthesize.py
+++ routers/synthesize.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Query
+from fastapi.responses import JSONResponse, Response
 from services.tts import synthesize_text_and_play_audio
 
 router = APIRouter()
 
-@router.post("/synthesize")
-async def play_audio_api(text: str, language: str):
+@router.get("/synthesize")
+async def play_audio_api(text: str = Query(...), language: str = Query(...)):
     try:
-        synthesize_text_and_play_audio(text, language)
+        wav_bytes = synthesize_text_and_play_audio(text, language)
+        return Response(content=wav_bytes, media_type="audio/wav")
+
         return JSONResponse(status_code=200, content={"message": "Audio played successfully"})
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
--- services/tts/synthesis.py
+++ services/tts/synthesis.py
@@ -1,9 +1,16 @@
 from services.models import en_tts_model,de_tts_model
 from .utils import play_audio
-
+import io
+import soundfile as sf
 def synthesize_text_and_play_audio(text, language):
     if language=='en':
         synthesized_audio = en_tts_model.tts(text=text, speaker="p314")
     elif language=='de':
         synthesized_audio = de_tts_model.tts(text=text)
+    
+    buffer = io.BytesIO()
+    sf.write(buffer, synthesized_audio, 22050, format='WAV')
+    buffer.seek(0)
+    return buffer.read()
+
     play_audio(synthesized_audio)
